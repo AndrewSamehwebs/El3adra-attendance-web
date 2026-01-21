7863deb675aa28e6d6d29da045de242a2792395a
AndrewSamehwebs <kevinroberto6810@gmail.com>
2026-01-21 05:19:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -200,7 +200,7 @@ function MainDashboard() {
   return (
     <div className="min-h-screen p-6 bg-gray-100">
       <h1 className="text-4xl font-bold mb-6 text-center text-red-900">
-        الأقسام
+        😉 اختار صف ولادك يا عدراوي
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
